785f94f8bad65e2244a8900d61af832029f90a7a
Dorian Baudry <dobaudry19@gmail.com>
2018-12-26 22:36:30 +0100

    Add a class for finite set (Alg 1. in Russo & Van Roy

--- expe.py
+++ expe.py
@@ -39,11 +39,29 @@ def sanity_check_expe():
         print(my_MAB.MAB)
         print(my_MAB.Cp)
         plt.subplot(121 + i)
-        plt.plot(my_MAB.MC_regret(method='UCB1', N=N1, T=1000, rho=0.2), label='UCB1')
-        plt.plot(my_MAB.MC_regret(method='Random', N=N1, T=1000, rho=0.), label='Random')
+        plt.plot(my_MAB.MC_regret(method='UCB1', N=N1, T=1000, param=0.2), label='UCB1')
+        plt.plot(my_MAB.MC_regret(method='Random', N=N1, T=1000, param=0.), label='Random')
         plt.plot(my_MAB.MC_regret(method='TS', N=N1, T=1000), label='TS')
         plt.plot(my_MAB.Cp * np.log(np.arange(1, 1001)))
         plt.ylabel('Cumulative Regret')
         plt.xlabel('Rounds')
         plt.legend()
+    plt.show()
+
+def check_finite(prior, q, R,theta, N, T):
+    method = ['F']*q.shape[1]
+    param = [[np.arange(q.shape[2]), q[theta, i, :]] for i in range(q.shape[1])]
+    my_MAB = mab.FiniteSets(method, param, q, prior, R)
+    print(prior)
+    print(R)
+    print(my_MAB.Ta)
+    param2 = [[R, q[theta, i, :]] for i in range(q.shape[1])]
+    check_MAB = mab.GenericMAB(method, param2)
+    regret_IDS = my_MAB.MC_regret(method='IDS', N=N, T=T, param=0.2)
+    plt.plot(regret_IDS, label='IDS')
+    plt.plot(check_MAB.MC_regret(method='UCB1', N=N, T=T, param=0.2), label='UCB1')
+    plt.plot(check_MAB.MC_regret(method='TS', N=N, T=T), label='TS')
+    plt.ylabel('Cumulative Regret')
+    plt.xlabel('Rounds')
+    plt.legend()
     plt.show()
