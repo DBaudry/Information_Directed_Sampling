b0540c37ef4a81e1937c6ed5b585118fbdce1a45
YRussac <russacyoan@gmail.com>
2018-12-30 22:35:30 +0100

    Adding functions for generating finite MAB and a deterministic finite MAB

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import numpy as np
 
 np.random.seed(123)
 if __name__ == '__main__':
-    R = np.linspace(0., 1., 11)
-    q = np.random.uniform(size=(2, 5, 11))
-    for i in range(q.shape[0]):
-        q[i] = np.apply_along_axis(lambda x : x/x.sum(), 1, q[i])
-    p = np.array([0.35, 0.65])
+    p, q, R = exp.build_finite_deterministic()
     exp.check_finite(p, q, R, theta=0, N=10, T=1000)
 
