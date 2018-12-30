06b78484e16aa704e9ae8eef1ff56b32b30ce5a9
YRussac <russacyoan@gmail.com>
2018-12-30 22:34:35 +0100

    Docstring correct syntax

--- MAB.py
+++ MAB.py
@@ -373,13 +373,14 @@ class BetaBernoulliMAB(GenericMAB):
 
 class FiniteSets(GenericMAB):
     def __init__(self, method, param, q_theta, prior, R):
-        '''
+        """
         theta in [1,L], Y in [1,N], A in [1,K]
+        K is the number of arms in our algorithm and is denoted nb_arms
         :param method: list with the types for each arm
         :param param: list with the parameters for each arm
         :param q_theta: L*K*N array with the probability of each outcome knowing theta
         :param R: mapping between outcomes and rewards
-        '''
+        """
         super().__init__(method, param)
         self.means = [(R*p[1]).sum() for p in param]
         self.mu_max = max(self.means)
@@ -392,10 +393,10 @@ class FiniteSets(GenericMAB):
         self.Ta = self.get_theta_a()
 
     def get_theta_a(self):
-        '''
+        """
         :return: list of length K containing the lists of theta for which action a in [1,K] is optimal
-        '''
-        Ta = [[] for a in range(self.nb_arms)]
+        """
+        Ta = [[] for _ in range(self.nb_arms)]
         for theta in range(self.L):
             a_theta = rd_argmax(np.dot(self.q_theta[theta, :, :], self.R))
             Ta[a_theta].append(theta)
