ba4d1fc962af262c3b573f461697fa69ade9dc64
YRussac <russacyoan@gmail.com>
2019-01-03 17:51:29 +0100

    generation bug

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ import MAB as mab
 
 np.random.seed(123)
 if __name__ == '__main__':
-    exp.beta_bernoulli_expe(T=1000, n_expe=10, n_arms=10, doplot=True)
+    p, q, R = exp.build_finite_deterministic()
+    # p, q, R = exp.build_finite(L=25, K=4, N=3)
+    exp.check_finite(p, q, R, theta=0, N=1, T=10)
+    # exp.beta_bernoulli_expe(T=1000, n_expe=10, n_arms=10, doplot=True)
