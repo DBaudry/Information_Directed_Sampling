1576636d47e53f0f53ef8823013e3aaeaf826495
YRussac <russacyoan@gmail.com>
2019-01-03 15:23:25 +0100

    RaiseError when argmax of an empty np.array

--- utils.py
+++ utils.py
@@ -5,7 +5,10 @@ import random as rd
 
 def rd_argmax(vector):
     """ Argmax that chooses randomly among eligible maximum indices. """
-    m = np.amax(vector)
-    indices = np.nonzero(vector == m)[0]
-    return rd.choice(indices)
+    if vector.size == 0:
+        raise IndexError("argmax of an empty array")
+    else:
+        m = np.amax(vector)
+        indices = np.nonzero(vector == m)[0]
+        return rd.choice(indices)
 
